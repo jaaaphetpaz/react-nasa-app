1e3d74025ada2bded7f2da4894636dd28379b701
jaaaphetpaz <102007135+jaaaphetpaz@users.noreply.github.com>
2024-09-30 13:04:35 -0400

    minor fix ";"

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ export default function Main(props) {
 
   return (
     <div className="imgContainer">
-      <img className="bgImage" src={data.hdurl} alt={data.title || "bg-img"} />;
+      <img className="bgImage" src={data.hdurl} alt={data.title || "bg-img"} />
     </div>
   );
 }
